ce172cba9083007cc15181079e1c334e7f9c2f38
drewjd27 <andrewdamanik23@gmail.com>
2025-06-13 18:07:54 +0700

    update modelling and mlproject

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,34 +4,32 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+def main():
 
-mlflow.set_experiment("modelling-Andrew")
+    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+    mlflow.set_experiment("modelling-Andrew")
+    mlflow.sklearn.autolog()
 
-# Aktifkan autolog
-mlflow.sklearn.autolog()
+    train_df = pd.read_csv('penguins_train_preprocessing.csv')
+    test_df = pd.read_csv('penguins_test_preprocessing.csv')
 
-# Load dataset preprocessed
-train_df = pd.read_csv('penguins_train_preprocessing.csv')
-test_df = pd.read_csv('penguins_test_preprocessing.csv')
+    X_train = train_df.drop(columns='species')
+    y_train = train_df['species']
+    X_test = test_df.drop(columns='species')
+    y_test = test_df['species']
 
-# Pisahkan fitur dan target
-X_train = train_df.drop(columns='species')
-y_train = train_df['species']
-X_test = test_df.drop(columns='species')
-y_test = test_df['species']
+    with mlflow.start_run(run_name="RandomForest"):
+        model = RandomForestClassifier(random_state=42)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
-# Start MLflow experiment run
-with mlflow.start_run(run_name="RandomForest"):
-    # Inisialisasi dan latih model
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+        acc = accuracy_score(y_test, y_pred)
+        print("Accuracy:", acc)
+        print("Classification Report:")
+        print(classification_report(y_test, y_pred))
 
-    # Prediksi
-    y_pred = model.predict(X_test)
+        # Log model secara eksplisit (untuk kebutuhan Docker image)
+        mlflow.sklearn.log_model(model, "model")
 
-    # Evaluasi
-    acc = accuracy_score(y_test, y_pred)
-    print("Accuracy:", acc)
-    print("Classification Report:")
-    print(classification_report(y_test, y_pred))
+if __name__ == "__main__":
+    main()
